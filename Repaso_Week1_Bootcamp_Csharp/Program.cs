550eecde13a3601cb974d2663f7cf880d2c69a81
JesusQP <andregallegosq85@gmail.com>
2025-04-04 21:03:20 -0500

    cambio realizado

--- Repaso_Week1_Bootcamp_Csharp/Program.cs
+++ Repaso_Week1_Bootcamp_Csharp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");  
+﻿namespace Repaso_Week1_Bootcamp_Csharp;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		int primerNumero;
+		int segundoNumero;
+		int tercerNumero;
+		int mayor;
+		Console.WriteLine("");
+		Console.WriteLine("============================");
+		Console.WriteLine("Cual es el numero mayor de 3 numeros ");
+		Console.WriteLine("============================");
+		Console.WriteLine("Ingrese los 3 numeros ");
+		primerNumero = Convert.ToInt32(Console.ReadLine());
+		segundoNumero = Convert.ToInt32(Console.ReadLine());
+		tercerNumero = Convert.ToInt32(Console.ReadLine());
+
+		if (primerNumero > segundoNumero)
+		{
+			mayor = primerNumero;
+		}
+		else
+		{
+			mayor=segundoNumero;
+		}
+		if (mayor > tercerNumero)
+		{
+			Console.WriteLine("Cel número mayor es: " + mayor);
+		}
+		else
+		{
+			Console.WriteLine("El número mayor es: " + tercerNumero);
+		}
+		/*
+			// Escribir la logica del programa
+			Console.WriteLine("");
+		Console.WriteLine("============================");
+		Console.WriteLine("Dame la opcion de que area es para comprobar ");
+		Console.WriteLine("============================");*/
+	}
+}
