15bdc58851b5db0878728845a4a0544d138a742d
Victor H <victor.huamandiaz@gmail.com>
2025-04-13 23:10:30 -0500

    Inicio de tareas del 1 al 4

---
+++ Tareas_Modulo1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Tareas_Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("     TAREAS DEL MODULO 1");
+            Console.WriteLine("============================");
+
+            Console.WriteLine("Ingrese opciòn de tareas:");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("( 1 ) : La Calculadora");
+            Console.WriteLine("( 2 ) : Reto de Bucles)");
+            Console.WriteLine("( 3 ) : Reto de Arrays");
+            Console.WriteLine("( 4 ) : Buscar Duplicados en un Array");
+            Console.WriteLine("( 5 ) : Salir");
+
+            int opcion;
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Es opcion 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Es opcion 1");
+                    break;
+                case 3:
+                    Console.WriteLine("Es opcion 1");
+                    break;
+                case 4:
+                    Console.WriteLine("Es opcion 1");
+                    break;
+                case 5:
+                    Console.WriteLine("Es opcion 1");
+                    break;
+                default:
+                    Console.WriteLine("No es opcoin valida");
+                    break;
+            }
+
+            Console.WriteLine("Saliendo de la aplicacion");
+
+
+
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tareas_Modulo1/Tarea1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareas_Modulo1
+{
+    internal class Tarea1
+    {
+    }
+}
