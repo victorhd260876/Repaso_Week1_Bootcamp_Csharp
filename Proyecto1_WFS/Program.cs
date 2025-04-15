8aa705067bd5b80352d9eb43a9875b4315c70a3b
Victor H <victor.huamandiaz@gmail.com>
2025-04-14 19:46:09 -0500

    ingreso de comentarios

--- Proyecto1_WFS/Program.cs
+++ Proyecto1_WFS/Program.cs
@@ -6,42 +6,46 @@ using System.Threading.Tasks;
 
 namespace Password
 {
-    class Ejecutar
+    //inicio de clase Ejecutar    
+    internal class Ejecutar
     {
         static void Main(string[] args)
         {
             int longitud;
-            Console.WriteLine("----------Generador de contraseñas----------");
+            Console.WriteLine("----------GENERADOR DE CONTRASEÑA ALEATORIA----------");
             Console.Write("\nIngrese la longitud de la contraseña: ");
             longitud = int.Parse(Console.ReadLine());
-
+            //imprime valor de la contraseña generada
             Console.WriteLine("La contraseña generada es: " + Password.GenerarPassword(longitud));
-
+            //permite ver el valor en la consola
             Console.ReadKey();
         }
     }
 
+    //inicio de clase password
     public static class Password
     {
         public static string GenerarPassword(int longitud)
         {
+            //declaracion de variables
             string contraseña = string.Empty;
             string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            
             Random EleccionAleatoria = new Random();
 
             for (int i = 0; i < longitud; i++)
             {
-                int LetraAleatoria = EleccionAleatoria.Next(0, 100);
-                int NumeroAleatorio = EleccionAleatoria.Next(0, 9);
+                int LetraAleat = EleccionAleatoria.Next(0, 100);
+                int NumeroAleat = EleccionAleatoria.Next(0, 9);
 
-                if (LetraAleatoria < letras.Length)
+                if (LetraAleat < letras.Length)
                 {
-                    contraseña += letras[LetraAleatoria];
+                    contraseña += letras[LetraAleat];
                 }
                 else
                 {
-                    contraseña += NumeroAleatorio.ToString();
+                    contraseña += NumeroAleat.ToString();
                 }
             }
             return contraseña;
