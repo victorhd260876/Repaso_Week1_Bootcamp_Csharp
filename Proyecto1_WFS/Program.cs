3f87bad378c7eda35a4c5a7c7c72fe3462abaa8e
Victor H <victor.huamandiaz@gmail.com>
2025-04-14 19:06:25 -0500

    Proyecto de generador de contraseña

---
+++ Proyecto1_WFS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password
+{
+    class Ejecutar
+    {
+        static void Main(string[] args)
+        {
+            int longitud;
+            Console.WriteLine("----------Generador de contraseñas----------");
+            Console.Write("\nIngrese la longitud de la contraseña: ");
+            longitud = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La contraseña generada es: " + Password.GenerarPassword(longitud));
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class Password
+    {
+        public static string GenerarPassword(int longitud)
+        {
+            string contraseña = string.Empty;
+            string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            Random EleccionAleatoria = new Random();
+
+            for (int i = 0; i < longitud; i++)
+            {
+                int LetraAleatoria = EleccionAleatoria.Next(0, 100);
+                int NumeroAleatorio = EleccionAleatoria.Next(0, 9);
+
+                if (LetraAleatoria < letras.Length)
+                {
+                    contraseña += letras[LetraAleatoria];
+                }
+                else
+                {
+                    contraseña += NumeroAleatorio.ToString();
+                }
+            }
+            return contraseña;
+        }
+    }
+}
