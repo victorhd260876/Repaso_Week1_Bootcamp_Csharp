02daea8abd03d01dd96ff540f645f2cb1ff90e8d
Victor H <victor.huamandiaz@gmail.com>
2025-04-14 20:04:13 -0500

    detallando comentarios

--- Proyecto1_WFS/Program.cs
+++ Proyecto1_WFS/Program.cs
@@ -12,10 +12,13 @@ namespace Password
         static void Main(string[] args)
         {
             int longitud;
-            Console.WriteLine("----------GENERADOR DE CONTRASEÑA ALEATORIA----------");
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("GENERADOR DE CONTRASEÑA ALEATORIA");
+            Console.WriteLine("=====================================================");
+            
             Console.Write("\nIngrese la longitud de la contraseña: ");
             longitud = int.Parse(Console.ReadLine());
-            //imprime valor de la contraseña generada
+            //imprime valor de la contraseña generada considerando el valor ingresado y llamando a la clase password
             Console.WriteLine("La contraseña generada es: " + Password.GenerarPassword(longitud));
             //permite ver el valor en la consola
             Console.ReadKey();
@@ -31,23 +34,28 @@ namespace Password
             string contraseña = string.Empty;
             string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-            
+            //instancia para generador clase aleatorio
             Random EleccionAleatoria = new Random();
-
+            //generando cada uno de los caracteres para construir la contraseña
             for (int i = 0; i < longitud; i++)
             {
+                //genera numero aleatorio entre el metodo next
                 int LetraAleat = EleccionAleatoria.Next(0, 100);
-                int NumeroAleat = EleccionAleatoria.Next(0, 9);
 
+                int NumeroAleat = EleccionAleatoria.Next(0, 9);
+                //si LetraAleat es menor al largo del array letras
                 if (LetraAleat < letras.Length)
                 {
+                    //se suma a la cadena de la variable contraseña
                     contraseña += letras[LetraAleat];
                 }
                 else
                 {
+                    //caso contrario el numero aleatorio se suma a la cadena de la variable contraseña
                     contraseña += NumeroAleat.ToString();
                 }
             }
+            //Console.WriteLine("CONTRASEÑA GENERADA");
             return contraseña;
         }
     }
